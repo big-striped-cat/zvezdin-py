06f0fb10f403b0d4d95192c693966dd0e714bf59
Evgeny Gusarov <p3gus@yandex.ru>
2023-12-02 01:18:58 +0300

    Reset window on emergency

--- backtest.py
+++ backtest.py
@@ -19,13 +19,16 @@ def backtest_strategy(
     order_list = order_manager.order_list
     local_broker = LocalBroker(order_list)
 
+    kline = None
     kline_window = []
     detector = EmergencyDetector()
 
-    # window consists of `window_size` historical klines and one current kline
-    for kline_window in get_moving_window_iterator(broker.klines(), window_size + 1):
-        # current kline
-        kline = kline_window[-1]
+    for kline in broker.klines():
+        if len(kline_window) < window_size:
+            kline_window.append(kline)
+            continue
+        kline_window.append(kline)
+        # now window consists of `window_size` historical klines and one current kline
 
         for order_id in local_broker.find_orders_for_auto_close(kline.open_time):
             logger.info('Order id=%s will be auto closed because of timeout', order_id)
@@ -36,18 +39,9 @@ def backtest_strategy(
         for event in broker.events(kline):
             local_broker.handle_remote_event(event)
 
-        prev_cooldown = detector.cooldown
         if detector.detect(kline_window):
-            if not prev_cooldown:
-                logger.warning('Emergency detected at %s', kline.open_time)
-            continue
-
-        if detector.cooldown:
-            logger.debug('Emergency detector cooling down')
-            continue
-
-        if prev_cooldown:
-            logger.info('Emergency has ended at %s', kline.open_time)
+            logger.warning('Emergency detected at %s', kline.open_time)
+            kline_window = []
             continue
 
         # pass historical klines
@@ -65,8 +59,12 @@ def backtest_strategy(
             event = broker.add_order(order)
             local_broker.add_order(event.order_id, order)
 
-    assert kline_window, 'Not enough klines'
-    last_price = kline_window[-1].close
+        kline_window.pop(0)
+
+    if len(kline_window) < window_size:
+        raise RuntimeError('Not enough klines')
+
+    last_price = kline.close
 
     logger.info(f'total orders open: {len(order_list.orders_open)}')
     logger.info(f'total orders closed: {len(order_list.orders_closed)}')
